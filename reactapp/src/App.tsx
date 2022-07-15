7b0de7a829473854aebb4dcf264833e3b93adf27
shigeru.hyodo <41170561gel1123@users.noreply.github.com>
2022-07-15 16:21:42 +0900

    chore: QA001読み込み

--- reactapp/src/App.tsx
+++ reactapp/src/App.tsx
@@ -1,13 +1,15 @@
 import { Container } from '@mui/system';
 import './App.css';
-import { SampleReactHookForm } from './components/SampleReactHookForm';
-import { SampleYupForm } from './components/SampleYupForm';
+// import { SampleReactHookForm } from './components/SampleReactHookForm';
+// import { SampleYupForm } from './components/SampleYupForm';
+import { QA001 } from './components/QA001';
 
 function App() {
   return (
     <Container style={{paddingTop: 10}}>
-      <SampleReactHookForm />
-      <SampleYupForm />
+      {/* <SampleReactHookForm />
+      <SampleYupForm /> */}
+      <QA001 />
     </Container>
   );
 }
